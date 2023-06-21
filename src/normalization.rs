9ba14e174c0c0485101576bb72abe72af72622dd
Andreas Maier <andreas.martin.maier@desy.de>
2023-06-21 16:34:40 +0200

    support deserialisation of positive cross section normalization

--- src/normalization.rs
+++ src/normalization.rs
@@ -7,6 +7,7 @@ use crate::ParseErr;
 pub struct Normalization {
     #[serde(rename = "@name")]
     pub name: String,
+    //#[serde(rename = "$field")]
     #[serde(rename = "XSection")]
     pub xsection: XSection,
     #[serde(rename = "Contribution")]
@@ -15,16 +16,54 @@ pub struct Normalization {
     pub number_of_rejected_events: String,
 }
 
+// // TODO: this would be the proper way to do this, but it doesn't work
+// #[derive(Deserialize, Serialize)]
+// #[derive(Clone, Debug, PartialEq, PartialOrd)]
+// #[serde(untagged)]
+// pub enum XSection {
+//     Neg(XSectionNeg),
+//     Pos(XSectionPos),
+// }
+
+// #[derive(Deserialize, Serialize)]
+// #[derive(Clone, Debug, PartialEq, PartialOrd)]
+// #[serde(rename_all = "PascalCase")]
+// pub struct XSectionNeg {
+//     #[serde(rename = "XSNeg")]
+//     pub xs_neg: XSScale,
+//     pub max_weight_neg: f64,
+//     pub total_events_neg: u64,
+//     pub accepted_events_neg: u64,
+//     pub factor_neg: String,
+// }
+
+// #[derive(Deserialize, Serialize)]
+// #[derive(Clone, Debug, PartialEq, PartialOrd)]
+// #[serde(rename_all = "PascalCase")]
+// pub struct XSectionPos {
+//     #[serde(rename = "XSPos")]
+//     pub xs_pos: XSScale,
+//     pub max_weight_pos: f64,
+//     pub total_events_pos: u64,
+//     pub accepted_events_pos: u64,
+//     pub factor_pos: String,
+// }
+
 #[derive(Deserialize, Serialize)]
 #[derive(Clone, Debug, Default, PartialEq, PartialOrd)]
 #[serde(rename_all = "PascalCase")]
 pub struct XSection {
-    #[serde(rename = "XSNeg")]
-    pub xs_neg: XSScale,
-    pub max_weight_neg: f64,
-    pub total_events_neg: u64,
-    pub accepted_events_neg: u64,
-    pub factor_neg: String,
+    // TODO: the `alias` is an evil hack and breaks serialization
+    #[serde(rename = "XSPos", alias = "XSNeg")]
+    pub xs_pos: XSScale,
+    #[serde(alias = "MaxWeightNeg")]
+    pub max_weight_pos: f64,
+    #[serde(alias = "TotalEventsNeg")]
+    pub total_events_pos: u64,
+    #[serde(alias = "AcceptedEventsNeg")]
+    pub accepted_events_pos: u64,
+    #[serde(alias = "FactorNeg")]
+    pub factor_pos: String,
 }
 
 #[derive(Deserialize, Serialize)]
@@ -86,13 +125,25 @@ mod tests {
     use super::*;
 
     #[test]
-    fn deser_xsection() {
+    fn deser_xsection_neg() {
         pub const REF_XS: &str = r#"<XSection>
  <XSNeg> 687.103,0.978277 </XSNeg>
  <MaxWeightNeg> 796475 </MaxWeightNeg>
  <TotalEventsNeg> 488338734 </TotalEventsNeg>
  <AcceptedEventsNeg> 493310 </AcceptedEventsNeg>
  <FactorNeg> 803.98,1.14468 </FactorNeg>
+</XSection>"#;
+        let _xs: XSection = quick_xml::de::from_str(REF_XS).unwrap();
+    }
+
+    #[test]
+    fn deser_xsection_pos() {
+        pub const REF_XS: &str = r#"<XSection>
+ <XSPos> 687.103,0.978277 </XSPos>
+ <MaxWeightPos> 796475 </MaxWeightPos>
+ <TotalEventsPos> 488338734 </TotalEventsPos>
+ <AcceptedEventsPos> 493310 </AcceptedEventsPos>
+ <FactorPos> 803.98,1.14468 </FactorPos>
 </XSection>"#;
         let _xs: XSection = quick_xml::de::from_str(REF_XS).unwrap();
     }
@@ -131,8 +182,17 @@ File generated with STRIPPER v0.1 for online data base
             norm.contribution.rw.rwentry,
             ["x1", "x2", "log(muR**2)", "log(muF**2)"]
         );
-        assert_eq!(norm.xsection.max_weight_neg, 796475.);
-        assert_eq!(norm.xsection.accepted_events_neg, 493310);
-        assert_eq!(norm.xsection.total_events_neg, 488338734);
+        let XSection {
+            xs_pos,
+            max_weight_pos,
+            total_events_pos,
+            accepted_events_pos,
+            factor_pos
+        } = norm.xsection;
+        assert_eq!(xs_pos, XSScale([687.103, 0.978277]));
+        assert_eq!(max_weight_pos, 796475.);
+        assert_eq!(accepted_events_pos, 493310);
+        assert_eq!(total_events_pos, 488338734);
+        assert_eq!(factor_pos, "803.98,1.14468");
     }
 }
